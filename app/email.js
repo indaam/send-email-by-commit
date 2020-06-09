cc14125fd98361ee794e19c189da70fa10ea04d4
indaam <indaam@gmail.com>
2020-06-09 11:23:17 +0700

    Update log

--- app/email.js
+++ app/email.js
@@ -7,72 +7,85 @@
  */
 
 const email = function(options){
-
+    
     const fs = options.fs;
     const nodemailer = options.nodemailer;
 
     this.data = options.data;
     this.config = options.config;
     this.helper = options.helper;
-    this.logDir = "_log/";
+    this.logDir = options.config.log.dir;
+    this.dir = options.dir;
+    this.child_process = options.child_process;
 
-    this.writeLog = function(){
+    this.writeLog = function(openBrowser){
         const today = this.helper.getToday(new Date());
         const names = ["html", "json"];
+        const logLocation = [];
         for( let i in names){
-            fs.writeFileSync( this.logDir + today + "." + names[i], (typeof this.data[names[i]] == "object" ? JSON.stringify(this.data[names[i]], null, 4) : this.data[names[i]] ));
+            const location = this.dir.root + "/" + this.dir.app + this.logDir + "/" + today + "." + names[i];
+            const content = (typeof this.data[names[i]] == "object" ? JSON.stringify(this.data[names[i]], null, 4) : this.data[names[i]] );
+            logLocation.push(location);
+            fs.writeFileSync( location, content);
             console.log('Create Log ' + today + "." + names[i] );
+        }
+
+        if(openBrowser){
+            return this.child_process.exec(`open -a "Google Chrome" ${logLocation[0]}`, (err, stdout, stderr) => {
+                console.log("stdout", stdout)
+            });
         }
     }
 
-    this.createLog = function(){
-
+    this.createLog = function(openBrowser){
         if (!fs.existsSync(this.logDir)){
             fs.mkdirSync(this.logDir);
-            this.writeLog();
+            this.writeLog(openBrowser);
         }else{
-            this.writeLog();
+            this.writeLog(openBrowser);
         }
 
     }
 
     this.send = function(){
-
-        this.createLog();
         const _this = this;
-
-        nodemailer.createTestAccount(function(err, account){
-            let transporter = nodemailer.createTransport({
-                host: 'smtp.gmail.com',
-                port: 465,
-                secure: true, // true for 465, false for other ports
-                auth: {
-                    user: _this.config.email.from, // generated ethereal user
-                    pass: _this.config.email.password // generated ethereal password
+        console.log("this.config", this.config);
+        const { autosend } = this.config.email;
+        if(autosend){
+            return nodemailer.createTestAccount(function(err, account){
+                let transporter = nodemailer.createTransport({
+                    host: 'smtp.gmail.com',
+                    port: 465,
+                    secure: true, // true for 465, false for other ports
+                    auth: {
+                        user: _this.config.email.user, // generated ethereal user
+                        pass: _this.config.email.password // generated ethereal password
+                    }
+                });
+    
+                let mailOptions = {
+                    from: '<'+ _this.config.email.user +'>',
+                    to: _this.data.json.to,
+                    subject: _this.data.json.subject,
+                    html: _this.data.html
+                };
+    
+                if( _this.config.email.cc ){
+                    mailOptions["cc"] = _this.config.email.cc;
                 }
+    
+                transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                        return console.log(error);
+                    }
+                    console.log('Message sent: %s', info.messageId);
+                    this.createLog(false);
+                });
             });
-
-            let mailOptions = {
-                from: '<'+ _this.config.email.from +'>',
-                to: _this.data.json.to,
-                subject: _this.data.json.subject,
-                html: _this.data.html
-            };
-
-            if( _this.config.email.cc ){
-                mailOptions["cc"] = _this.config.email.cc;
-            }
-
-            transporter.sendMail(mailOptions, function(error, info){
-                if (error) {
-                    return console.log(error);
-                }
-                console.log('Message sent: %s', info.messageId);
-                this.createLog();
-            });
-        });
-        
-
+        }
+        console.log("Open preview");
+        this.createLog(true);
+        return null;
     }
 }
 
